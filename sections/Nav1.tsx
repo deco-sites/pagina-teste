dadd5be7a5d5f2cb7ed682ea2f671a54b9830897
decobot <wubignermairinque@gmail.com>
2024-06-26 00:41:15 +0000

    Release staging environment with 0 changes made by

--- sections/Nav1.tsx
+++ sections/Nav1.tsx
@@ -10,11 +10,11 @@ const NavigationBar = () => {
     { label: 'INICIO', href: '#inicio' },
     { label: 'QUEM SOMOS', href: 'quemsomos' },
     { label: 'QUERO MIGRAR', href: 'queromigrar' },
-    { label: 'CONTATO!', href: 'contato' },
+    { label: 'CONTATO', href: 'contato' },
   ];
 
   return (
-    <nav className="sticky top-0 z-50 flex items-center justify-between text-white w-full px-4 py-4 bg-indigo-500">
+    <nav className="sticky top-0 z-50 flex items-center justify-center text-white w-full px-4 py-4 bg-indigo-500">
       <ul className="flex space-x-4">
         {navigationItems.map((item, index) => (
           <li key={item.label}>
