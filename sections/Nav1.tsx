36be37ab7b106dfb993193c5f6b9eb322543fbb5
decobot <wubignermairinque@gmail.com>
2024-06-26 00:26:24 +0000

    Release staging environment with 1 changes made by wubignermairinque@gmail.com

--- sections/Nav1.tsx
+++ sections/Nav1.tsx
@@ -1,52 +1,35 @@
-import { ImageWidget } from "apps/admin/widgets.ts";
-import Image from "apps/website/components/Image.tsx";
+// NavigationBar.tsx (Deco component)
 
-interface Props {
-  href?: string;
-  image?: ImageWidget;
-  alt?: string;
-  width?: number;
-  height?: number;
-  text?: string;
-  list?: {
-    listItem: string;
-    listItemArray: string[];
-  };
+interface NavItem {
+  label: string;
+  href: string;
 }
 
-export default function Footer(
-  { image, href, text, alt, height, width, list }: Props,
-) {
-  const defaultImage =
-    "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4959/d7aa9290-074f-417c-99c3-5b0587c8c2ee";
-  const defaultHref = "https://deco.cx/";
-  const defaultText = "Made with";
-  const defaultAlt = "Made with deco.cx";
-  const defaultHeight = 20;
-  const defaultWidth = 50;
-  const defaultList = {
-    listItem: "1",
-    listItemArray: ["1", "2"],
-  };
+const NavigationBar = () => {
+  const navigationItems: NavItem[] = [
+    { label: 'INICIO', href: '#inicio' },
+    { label: 'QUEM SOMOS', href: 'quemsomos' },
+    { label: 'QUERO MIGRAR', href: 'queromigrar' },
+    { label: 'CONTATO!', href: 'contato' },
+  ];
 
   return (
-    <div class="py-8 lg:px-0 px-6 fixed bottom-0 w-full mx-auto">
-      <a
-        href={href || defaultHref}
-        class="flex flex-row gap-1 items-center justify-center text-xs"
-        target="_blank"
-      >
-        {text && <p>{text || defaultText}</p>}
-        {list && <div>{list || defaultList}</div>}
-        {image && (
-          <Image
-            src={image || defaultImage}
-            alt={alt || defaultAlt}
-            height={height || defaultHeight}
-            width={width || defaultWidth}
-          />
-        )}
-      </a>
-    </div>
+    <nav className="sticky top-0 z-50 flex items-center justify-between text-white w-full px-4 py-4 bg-indigo-500">
+      <ul className="flex space-x-4">
+        {navigationItems.map((item, index) => (
+          <li key={item.label}>
+            <a
+              href={item.href}
+              className={`px-4 py-2 text-white hover:bg-indigo-200 hover:text-yellow-200 ${index === navigationItems.length - 1 ? '' : 'border-r-2 border-white'
+                }`}
+            >
+              {item.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
-}
+};
+
+export default NavigationBar;
