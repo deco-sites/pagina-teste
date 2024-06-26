9422edb92c828640c453c4c55bbb8f2266db9085
decobot <wubignermairinque@gmail.com>
2024-06-26 04:00:52 +0000

    Release staging environment with 1 changes made by wubignermairinque@gmail.com

--- sections/Nav1.tsx
+++ sections/Nav1.tsx
@@ -10,11 +10,40 @@ const NavigationBar = () => {
     { label: 'INICIO', href: '#inicio' },
     { label: 'QUEM SOMOS', href: 'quemsomos' },
     { label: 'QUERO MIGRAR', href: 'queromigrar' },
-    { label: 'CONTATO', href: 'contato' },
+    { label: 'CONTATO!', href: 'contato' },
   ];
 
+/**
   return (
-    <nav className="sticky top-0 z-50 flex items-center justify-center text-white w-full px-4 py-4 bg-indigo-500">
+    <nav className="sticky top-0 z-50 flex items-center justify-center mx-auto w-full px-4 py-4 bg-indigo-500">
+      <ul className="flex space-x-4">
+        {navigationItems.map((item, index) => (
+          <li key={item.label}>
+            <a
+              href={item.href}
+              className={`px-4 py-2 text-white hover:bg-indigo-200 hover:text-yellow-200 ${
+                index === navigationItems.length - 1 ? '' : 'border-r-2 border-white'
+              }`}
+            >
+              {item.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavigationBar;
+
+
+*/
+
+  return (
+    <nav
+      className={`sticky top-0 z-50 flex items-center justify-center mx-auto w-full px-4 py-4 bg-indigo-500 ${window.scrollY > 0 ? '@apply sticky' : ''
+        }`}
+    >
       <ul className="flex space-x-4">
         {navigationItems.map((item, index) => (
           <li key={item.label}>
