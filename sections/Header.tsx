fab4b5bb9fc5be7934eef1f079dd966a0e5eca2c
decobot <wubignermairinque@gmail.com>
2024-06-27 04:10:51 +0000

    Release staging environment with 1 changes made by wubignermairinque@gmail.com

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -3,13 +3,7 @@ import Image from "apps/website/components/Image.tsx";
 import Icon from "../components/ui/Icon.tsx";
 
 const Rectangle2 = () => (
-  <div className="rectangle_2 flex-shrink-0 w-[17.5625rem] h-[3.125rem] rounded-[1.1875rem] border border-[#0085ff] bg-[#0c1126]" />
+  <div className="rectangle_2 flex-shrink-0 w-[783px] h-[1.5625rem] text-white font-['Inter'] text-xl font-bold leading-[normal] rounded-[1.1875rem] border border-[#0085ff] bg-[#0c1126]" />
 );
 
 export default Rectangle2;
-
-const InicioQuemSomosContatoPrecosPorQueMigrar = () => (
-  <div className="inicio___quem_somos___contato___pre_os___por_que_migrar_ flex-shrink-0 w-[783px] h-[1.5625rem] text-white font-['Inter'] text-xl font-bold leading-[normal]">INICIO | QUEM SOMOS | CONTATO | PREÇOS | POR QUE MIGRAR?</div>
-);
-
-export default InicioQuemSomosContatoPrecosPorQueMigrar;
