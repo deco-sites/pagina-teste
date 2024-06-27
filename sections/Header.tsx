cc5efc951aa5d9fdf2b57eee05f959eff6e339fe
decobot <wubignermairinque@gmail.com>
2024-06-27 12:40:59 +0000

    Release staging environment with 1 changes made by wubignermairinque@gmail.com

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,11 +1,11 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 import Icon from "../components/ui/Icon.tsx";
-import Nav1 from './Nav1'; 
+import Nav1 from './Nav1.tsx'; 
 
 const Rectangle2 = () => (
-  <div className="rectangle_2 flex-shrink-0 w-[783px] h-[1.5625rem] text-white font-['Inter'] text-xl font-bold leading-[normal] rounded-[1.1875rem] border border-[#0085ff] bg-[#0c1126]">
-  <Nav1/>
+  <div className="rectangle_2 flex-shrink-0 w-[783px] h-[1.5625rem] text-white font-['Inter'] text-xl font-bold leading-[normal] rounded-[1.1875rem] border border-[#0085ff] bg-[#050B1D]">
+  <Nav1 />
   </div>
 );
 
