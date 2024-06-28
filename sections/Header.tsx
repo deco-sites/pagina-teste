aede13cb262c6bcd2ffb8210cd4b5a0b89ee8220
decobot <wubignermairinque@gmail.com>
2024-06-28 14:53:53 +0000

    Release staging environment with 1 changes made by wubignermairinque@gmail.com

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -8,13 +8,16 @@ const Rectangle2 = () => (
 
 /**  <div className="rectangle_2 flex-shrink-0 w-[783px] h-[1.5625rem] text-white font-['Inter'] text-xl font-bold leading-[normal] rounded-[1.1875rem] border border-[#0085ff] bg-[#050B1D]">
 */
-  <div className="rectangle_2 flex-shrink-0 w-[1520px] h-[140px] text-white font-['Inter'] text-xl font-bold leading-[normal] rounded-[1.1875rem] border border-[#0093ff] bg-[#050B1D] display:flex justify-content-center">
+  <div className="rectangle_2 flex-shrink-0 w-[1520px] h-[280px] text-white font-['Inter'] text-xl font-bold leading-[normal] rounded-[1.1875rem] border border-[#0093ff] bg-[#050B1D] display:flex justify-content-center">
   <Nav1 />
     <p>Um texto para preenchimento</p> {/* Add a paragraph */}
     <br /> {/* Add a line break */}
     <h1>Important Heading</h1> {/* Add a heading */}
     <p>Pàra testar a atualização do componente</p> {/* Another paragraph */}
     {/* You can add other HTML tags as needed */}
+    <div className="flex-item">
+    teste
+    </div>
   </div>
 );
 
