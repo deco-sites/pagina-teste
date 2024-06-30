e2cb3b90a613a36864f2b54362b4d0233f8988f1
decobot <wubignermairinque@gmail.com>
2024-06-30 05:33:31 +0000

    Release staging environment with 1 changes made by wubignermairinque@gmail.com

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -7,14 +7,14 @@ const Rectangle2 = () => (
   <div className="rectangle_2 flex w-[1520px] h-[280px] text-white font-['Inter'] text-xl font-bold leading-[normal] rounded-[1.1875rem] border border-[#0093ff] bg-[#050B1D]">
     <div class="elementos_header flex flex-col items-center justify-center h-screen">
       <div class="item border-secondary rounded-lg overflow-hidden">
-      <img src="imagem.jpg" alt="Imagem" class="w-full md:w-1/2 mb-8">
+      <img src="imagem.jpg" alt="Imagem" class="w-full md:w-1/2 mb-8"/>
       </div>
       <div class="item border-secondary rounded-lg overflow-hidden">
         <Nav1 class="flex flex-row gap-4"/>
       </div>
       <div class="item border-secondary rounded-lg overflow-hidden">
         <div class="flex flex-row gap-4 mt-8">
-          <input type="text" class="w-full border border-gray-300 rounded-md px-4 py-2" placeholder="Pesquisar">
+          <input type="text" class="w-full border border-gray-300 rounded-md px-4 py-2" placeholder="Pesquisar"/>
             <button type="button" class="bg-blue-500 text-white font-medium px-4 py-2 rounded-md hover:bg-blue-600">Pesquisar</button>
         </div>
       </div>
